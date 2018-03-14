b88caebaccfb9a3ae1610bada75911be8fc0cfaa
rabelais88 <sungryeolp@gmail.com>
2018-03-15 05:17:31 +0900

    dd

--- settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apikeys:{
-    facebook:'EAACEdEose0cBAK6zRRmOzf849okiAH1hhlz4ZChbL54df4nvXZAVioSty6pq1qONhaucRMksCNRfXtYLpLwgd1SH8BRAgwiifVhTjfg4V5YP2ymZAaMw9NcDCYajBjKQK9WI9PHB3A3VObbjdMRCszYMiFvRkkoBU3MaIXHzlmXP15eF67FtgWaRxqWZAZCsZD',
-    twitter:'http://twitter.com/ParkWebdev'
-  },
-  maxString:120
-}
